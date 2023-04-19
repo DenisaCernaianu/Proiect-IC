718c87a77b788934a330f444073bae81db42bb10
DenisaCernaianu <denisa.cernaianu@student.upt.ro>
2023-04-19 16:23:20 +0300

    add tour page, tour details

--- client/src/pages/Tours.jsx
+++ client/src/pages/Tours.jsx
@@ -1,7 +1,61 @@
-import React from "react"; //6.9k (gzipped: 2.7k)
+import React ,{useState, useEffect} from "react"; //6.9k (gzipped: 2.7k)
+import CommonSection from "../shared/CommonSection";
+import '../styles/tour.css';
+import TourCard from'./../shared/TourCard';
+import SearchBar from'./../shared/SearchBar';
+import Newsletter from'./../shared/Newsletter';
+import tourData from '../assets/data/tours';
+import { Container, Row, Col } from "reactstrap";
+
 
 const Tours = () => {
-    return <div>Tours</div>
+
+    const [pageCount, setPageCount] = useState(0)
+    const [page,setPage] = useState(0)
+
+    useEffect(()=>{
+        const pages = Math.ceil(5/ 4); //later we wil use backend data coount
+        setPageCount(pages)
+    },[page]);
+
+    return(
+    <>
+    <CommonSection title={"All Tours"} />
+    <section>
+        <Container>
+            <Row>
+                <SearchBar/>
+            </Row>
+        </Container>
+    </section>
+    <section className="pt-0">
+        <Container>
+            <Row>
+                {
+                    tourData?.map(tour=> (
+                        <Col lg='3' className="mb-4" key={tour.id}>
+                        <TourCard tour={tour}/>
+                        </Col>
+                        ))
+                }
+
+                <Col lg='12'>
+                    <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                        {[...Array(pageCount).keys()].map(number=>(
+                            <span key={number} onClick={()=> setPage(number)}
+                            className={page === number ? "active_page" : ""}
+                            >
+                                {number + 1}
+                            </span>
+                        ))}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+    <Newsletter/>
+    </>
+    );
 };
 
 export default Tours;
