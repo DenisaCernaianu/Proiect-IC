d835e88619a2bd7ec14a2bd15e43f682b067ff9e
DenisaCernaianu <denisa.cernaianu@student.upt.ro>
2023-04-22 17:34:27 +0300

    started backend for tours

---
+++ backend/controllers/tourController.js
@@ -0,0 +1,124 @@
+import Tour from "../models/Tour.js";
+
+//create new tour
+export const createTour = async(req,res)=>{
+    const newTour = new Tour(req.body);
+
+    try{
+       const savedTour = await newTour.save();
+
+       res.status(200).json({success:true, message:'Successfully created',data:savedTour});
+    }catch (err){
+        res.status(500).json({success:false, message:'Failed to be  created.Try again!'});
+
+    }
+};
+
+//update tour
+
+export const updateTour = async(req,res)=>{
+
+    const id = req.params.id
+    try {
+        
+        const updatedTour =  await Tour.findByIdAndUpdate(id, {
+            $set: req.body
+        }, {new:true})
+
+        res.status(200).json({success:true, message:'Successfully updated',data:updatedTour});
+
+    } catch (err) {
+        res.status(500).json({success:false, message:'Failed to be updated.Try again!'});
+    }
+};
+
+//delete tour
+export const deleteTour = async(req,res)=>{
+    const id = req.params.id
+    try {
+        
+        await Tour.findByIdAndDelete(id);
+
+        res.status(200).json({success:true, message:'Successfully deleted'});
+
+    } catch (err) {
+        res.status(500).json({success:false, message:'Failed to delete.Try again!'});
+    }
+};
+
+//getSingle tour
+export const getSingleTour = async(req,res)=>{
+    const id = req.params.id
+    try {
+        
+       const tour = await Tour.findById(id);
+
+        res.status(200).json({success:true, message:'Successfully found',data:tour});
+
+    } catch (err) {
+        res.status(404).json({success:false, message:'not found'});
+    }
+};
+  
+//getAllTour
+export const getAllTour = async(req,res)=>{
+         
+        //for pagination 
+        const page = parseInt(req.query.page);
+
+
+    try {
+
+        const tours = await Tour.find({}).skip(page * 8).limit(8);
+
+        res.status(200).json({
+            success:true, count:tours.length, message:'Successfully found',data:tours,
+        })
+        
+    } catch (err) {
+        res.status(404).json({success:false, message:'not found'});
+    }
+};
+
+
+//get tour by search 
+
+export const getTourBySearch = async(req,res)=>{
+    const city = new RegExp(req.query.city, 'i')    //here 'i' means case sensitive 
+    const distance = parseInt(req.query.distance)
+    const maxGroupSize = parseInt(req.query.maxGroupSize)
+
+    try {
+        //gte means greater than equal
+        const tours = await Tour.find({ city, distance:{$gte:distance}, maxGroupSize:{$gte:maxGroupSize}, })
+        
+        res.status(200).json({
+            success:true,  message:'Successfully found',data:tours,
+        })
+
+    } catch (err) {
+        res.status(404).json({success:false, message:'not found'});
+        
+    }
+
+
+}
+
+
+//get featured Tour
+export const getFeaturedTour = async(req,res)=>{
+
+
+try {
+
+    const tours = await Tour.find({featured:true}).limit(8);
+
+    res.status(200).json({
+        success:true, message:'Successfully found',data:tours,
+    })
+    
+} catch (err) {
+    res.status(404).json({success:false, message:'not found'});
+}
+};
+  
---
+++ backend/routes/tours.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { createTour , updateTour, deleteTour, getSingleTour, getAllTour, getTourBySearch, getFeaturedTour} from "./../controllers/tourController.js";
+
+const router = express.Router();
+
+// create new tour
+router.post("/",createTour);
+
+// update tour
+router.put("/:id",updateTour);
+
+// delete tour
+router.delete("/:id",deleteTour);
+
+// get single tour
+router.get("/:id",getSingleTour);
+
+// get all tours
+router.get("/",getAllTour);
+
+//get tour by search
+router.get("/search/getTourBySearch",getTourBySearch);
+router.get("/search/getFeaturedTours",getFeaturedTour);
+
+export default router;
+
